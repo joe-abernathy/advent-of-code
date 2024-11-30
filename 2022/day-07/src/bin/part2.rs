497b73d0f5f94b0f0b1ab5b66c9ce8aea99c8c5a
Joe Abernathy <jabernathy36@gmail.com>
2024-11-30 11:40:15 -0600

    Finished day 7 part 2

---
+++ 2022/day-07/src/bin/part2.rs
@@ -0,0 +1,233 @@
+/* 
+To complete this puzzle, I used nested hashmaps to model the file system. An outer hashmap represents
+the directories. This uses a string (representing the directory path) as a key, and a tuple with a u32
+(representing the directory's size) and an inner hashmap (storing the files in the directory). The inner
+hashmap has a string (representing the filename, no path) as a key, and a u32 (representing the filesize)
+as a value. 
+
+We iterate through all lines in the input, parsing commands and responses as needed. When we encounter a 
+new directory, we add a new key/value pair to the OUTER hashmap to represent that new directory. When we
+receive a response from "ls" indicating a new file, we add a new key/value pair to the INNER hashmap to 
+represent that file. When we do that, we also update the directory size field in the directory that 
+contains the file, as well as every parent . This is done by traversing up the directory structure until 
+we reach the root, incrementing the directory size for each.
+
+Once we finish parsing through the input, the hard part is over. A little bit of arithmetic to calculate
+the minimum directory size that we need to delete to free up the required amount of space, then find the
+minimum directory size in a vector of directories that meet that requirement.
+*/
+
+use aoc_helpers::{ * };
+use std::error::Error;
+use std::collections::HashMap;
+use itertools::Itertools;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let input = get_puzzle_input_as_lines(2022, 7)?;
+    let mut filesys: HashMap<String, (u32, HashMap<String, u32>)> = HashMap::new();
+
+    parse_inputs(input, &mut filesys);
+    let result = find_smallest_candidate_dir(filesys.clone());
+
+    println!("{}", result);
+
+    Ok(())
+}
+
+// Processes each line in the input 
+fn parse_inputs(input: Vec<String>, filesys: &mut HashMap<String, (u32, HashMap<String, u32>)>) {
+
+    // Points to the current directory we're working with, since we need to keep track of hierarchy and whatnot
+    let mut current_dir = String::new();
+    
+    for line in input {
+
+        // If the first character in the line is '$', process it as a command
+        if line.clone().chars().nth(0) == Some('$') {
+
+            // Get the command by trimming off the first 2 characters ('$ '), then split it by whitespace
+            let cmd: String = line.chars().skip(2).collect();
+            let split: Vec<&str> = cmd.split(' ').collect();
+
+            // split[0] is the first word of the command ("cd" or "ls")
+            match split[0] {
+                "cd" => {
+
+                    // split[1] is the second word of the command. This only applies for "cd" commands, since this
+                    // puzzle doesn't implement ls with any secondary commands
+                    match split[1] {
+
+                        // If the command is "cd ..", traverse up one level
+                        ".." => {
+                            current_dir = up_one_level(current_dir);
+                        },
+
+                        // If the command is "cd <anything else>", traverse down one level using the subdirectory provided
+                        _ => {
+                            current_dir = down_one_level(current_dir, split[1]);
+
+                            // Once we've updated the current_dir pointer, if the new directory doesn't exist in the hashmap
+                            // yet, add it now.
+                            if filesys.get(&current_dir) == None {
+                                let new_dir_map: HashMap<String, u32> = HashMap::new();
+                                filesys.insert(current_dir.clone(), (0, new_dir_map));
+                            }
+                        }
+                    }
+                }
+
+                // If the command is "ls", we don't need to do any more processing -- just skip to the next line where we will
+                // process the response(s).
+                "ls" => continue,
+
+                // If the first word is anything other than "cd" or "ls", something has gone horribly wrong
+                _ => panic!("Got an unexpected command: {}", split[0]),
+            }
+
+        // If the line doesn't start with a $ and is therefore a response from the 'ls' command:
+        } else {
+
+            // Split the response by words
+            let split_response: Vec<String> = line.split(' ')
+                .map(|s| s.to_string())
+                .collect();
+
+            match split_response[0].as_str() {
+
+                // If the first word of the response is "dir", the response is describing a subdirectory. The way I've implemented
+                // this, we don't need to do any more processing here, since the directory will be added to the hashmap elsewhere,
+                // so we just move on to the next line. 
+                "dir" => continue,
+
+                // If the first word isn't "dir", the response is describing a file, and will include two words: the size of the
+                // file, followed by the filename.
+                _ => {
+
+                    // Parse the first word (size) into an integer
+                    let size: u32 = split_response[0].parse::<u32>().expect("Expecting a string that can be parsed to an integer, didn't get it");
+
+                    // Get the second word (filename)
+                    let filename = split_response[1].clone();
+
+                    // Insert a new entry into the inner hashmap corresponding to the current directory. The new entry contains
+                    // the filename (key) and file size (value).
+                    if let Some((_, current_dir_map)) = filesys.get_mut(&current_dir) {
+                        current_dir_map.insert(filename.clone(), size);
+
+                        recursive_dir_size_update(filesys, current_dir.clone(), size);
+                    }
+                }
+            }
+        }
+    }
+}
+
+// Take a string pointing to the current directory and return a string pointing to its parent directory
+fn up_one_level(current_dir: String) -> String {
+
+    // If we're already at the root directory, just return without processing.
+    // This shouldn't ever happen, but just in case.
+    if current_dir == "/" {
+        return current_dir;
+    }
+
+    // Split the directory string by '/' characters and collect it into a vector for processing
+    let dir_vec: Vec<String> = current_dir.split('/')
+        .map(|s| s.to_string())
+        .collect();
+
+    // Chop off the last entry in the split vector (the current directory), then slap what's left
+    // back together, joined with "/"s
+    let parent_dir = dir_vec
+        .iter()
+        .take(dir_vec.len() - 2)
+        .map(|s| s.as_str())
+        .join("/");
+
+    // Add a trailing "/" and return
+    format!("{}{}", parent_dir, "/")
+}
+
+// Take a string pointing to the current directory and a string slice pointing to the subdirectory,
+// and return a string pointing to the subdirectory
+fn down_one_level(current_dir: String, subdir: &str) -> String {
+    if current_dir.is_empty() {
+        return "/".to_string();
+    }
+    let new_dir = format!("{}{}", subdir, "/");
+    format!("{}{}", current_dir, &new_dir)
+}
+
+// Whenever we add a new file, we need to update not only the directory size for the current directory, but also the directory
+// size for every parent directory going up to the root. This function traverses the directory upwards toward the root, adding
+// the new file's size to each directory's dir_size.
+fn recursive_dir_size_update(filesys: &mut HashMap<String, (u32, HashMap<String, u32>)>, current_dir: String, filesize: u32) {
+    
+    // Points to the current directory in the traversal
+    let mut dir = current_dir.clone();
+    
+    // Loop until we explictly break after reaching the root directory
+    loop {
+
+        // If the current directory already exists in the hashmap, add the current filesize to its dirsize
+        if let Some((dirsize, _)) = filesys.get_mut(&dir) {
+            *dirsize += filesize;
+        
+        // If the current directory isn't in the hashmap yet, insert it into the outer hashmap with an initial
+        // dirsize equal to the filesize
+        } else {
+            let parent_dir_map: HashMap<String, u32> = HashMap::new();
+            filesys.insert(dir.clone(), (filesize, parent_dir_map));
+        }
+        
+        // Break out of the loop once we've processed the root directory
+        if dir == "/" {
+            break;
+        }
+
+        // Traverse up one level in the file system and continue the loop
+        dir = up_one_level(dir);
+    }
+}
+
+// Find the smallest directory that can be deleted to get our free space up to 30,000,000
+fn find_smallest_candidate_dir(filesys: HashMap<String, (u32, HashMap<String, u32>)>) -> u32 {
+    let mut candidate_dirs: Vec<u32> = Vec::new();
+
+    // Get the size of the root directory (equal to the total used space in the file system)
+    let (root_size, _) = filesys.get("/").unwrap();
+
+    // Calculate the free space (total size of 70,000,000 minus the total used space)
+    let free_space = 70000000 - root_size;
+
+    // Calculate the minimum directory size we need to delete to get to free space of 30,000,000
+    let min_size = 30000000 - free_space;
+
+    // Iterate across all directories in the hashmap. If a directory's size is at least the
+    // minimum size that we need to delete, add it to the list of candidates.
+    for (dirsize, _) in filesys.values() {
+        if *dirsize >= min_size {
+            candidate_dirs.push(*dirsize);
+        }
+    }
+    
+    // Return the smallest directory size from the list of candidates
+    *candidate_dirs.iter().min().unwrap()
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn example() -> Result<(), Box<dyn Error>> {
+        let input = read_from_file_as_lines("example.txt");
+        let mut filesys: HashMap<String, (u32, HashMap<String, u32>)> = HashMap::new();
+
+        parse_inputs(input, &mut filesys);
+        let result = find_smallest_candidate_dir(filesys);
+
+        Ok(assert_eq!(result, 24933642))
+    }
+}
