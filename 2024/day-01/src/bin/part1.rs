7e7d1976fd9b99661815bf2ff70e55fee023d9d7
Joe Abernathy <jabernathy36@gmail.com>
2024-11-30 23:33:15 -0600

    Polished them a little, added some comments

--- 2024/day-01/src/bin/part1.rs
+++ 2024/day-01/src/bin/part1.rs
@@ -9,11 +9,15 @@ fn main() -> Result<(), Box<dyn Error>> {
     let input = get_puzzle_input_as_lines(2024, 1)?;
     let (mut left, mut right) = get_vecs_from_input(input);
     
+    // Sort the two vectors
     left.sort();
     right.sort();
 
+    // Running total of difference values
     let mut total: u32 = 0;
 
+    // Iterate across each entry in both vectors and get the absolute value of the difference
+    // between the two, then add it to the running total
     for i in 0..left.len() {
         total += left[i].abs_diff(right[i]);
     }
@@ -23,12 +27,14 @@ fn main() -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
+// Converts our original vector of strings to a pair of vectors of ints (left and right)
 fn get_vecs_from_input(input: Vec<String>) -> (Vec<u32>, Vec<u32>) {
     let mut left: Vec<u32> = Vec::new();
     let mut right: Vec<u32> = Vec::new();
 
     for line in input {
         let split: Vec<&str> = line.split("   ").collect();
+    
         left.push(split[0].parse::<u32>().expect("Failed to parse string to u32"));
         right.push(split[1].parse::<u32>().expect("Failed to parse string to u32"));
     }
--- 2024/day-01/src/bin/part2.rs
+++ 2024/day-01/src/bin/part2.rs
@@ -9,8 +9,11 @@ fn main() -> Result<(), Box<dyn Error>> {
     let input = get_puzzle_input_as_lines(2024, 1)?;
     let (left, right) = get_vecs_from_input(input);
     
+    // Running total
     let mut total = 0;
 
+    // For each entry in the left vector, count the number of times that value appears in the right 
+    // vector and then add it to the running total
     for i in 0..left.len() {
         let current_count = right
             .iter()
