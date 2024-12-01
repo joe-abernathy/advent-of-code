78d3297d879cee2f8d241f1f1b3e25d725eb01e0
Joe Abernathy <jabernathy36@gmail.com>
2024-11-30 23:26:51 -0600

    Finished part 2!

--- 2024/day-01/src/bin/part2.rs
+++ 2024/day-01/src/bin/part2.rs
@@ -1,3 +1,39 @@
-fn main() {
-    println!("Part 2!");
+use aoc_helpers::{ * };
+use std::error::Error;
+
+// Normally I'd try to make this a lot more polished than this, but I actually had a handle on the
+// problem within a few minutes of it dropping, so I was focused on speed rather than polish. I'll
+// clean this up later, but the important thing is that it works, okay??
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let input = get_puzzle_input_as_lines(2024, 1)?;
+    let (left, right) = get_vecs_from_input(input);
+    
+    let mut total = 0;
+
+    for i in 0..left.len() {
+        let current_count = right
+            .iter()
+            .filter(|&&x| x == left[i])
+            .count();
+
+        total += left[i] * current_count as u32;
+    }
+
+    println!("{}", total);
+
+    Ok(())
+}
+
+fn get_vecs_from_input(input: Vec<String>) -> (Vec<u32>, Vec<u32>) {
+    let mut left: Vec<u32> = Vec::new();
+    let mut right: Vec<u32> = Vec::new();
+
+    for line in input {
+        let split: Vec<&str> = line.split("   ").collect();
+        left.push(split[0].parse::<u32>().expect("Failed to parse string to u32"));
+        right.push(split[1].parse::<u32>().expect("Failed to parse string to u32"));
+    }
+
+    (left, right)
 }
