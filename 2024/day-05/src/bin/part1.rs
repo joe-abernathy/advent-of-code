66d380cf3b496aa255db6e2b442dbe4ac5f01946
Joe Abernathy <jabernathy36@gmail.com>
2024-12-07 08:35:19 -0600

    Cleaned up and commented day 5 part 1

--- 2024/day-05/src/bin/part1.rs
+++ 2024/day-05/src/bin/part1.rs
@@ -1,7 +1,6 @@
 use aoc_helpers::{ * };
 use std::error::Error;
 
-#[inline]
 fn main() -> Result<(), Box<dyn Error>> {
     let input = get_puzzle_input_as_lines(2024, 5)?;
     let (rules, pages) = process_input(input);
@@ -12,7 +11,14 @@ fn main() -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
-pub fn count_correct_pages(rules: Vec<(u32, u32)>, pages: Vec<Vec<u32>>) -> u32 {
+// I'm slowly getting the hang of iterators in Rust. Pretty impressed tbh. 
+fn count_correct_pages(rules: Vec<(u32, u32)>, pages: Vec<Vec<u32>>) -> u32 {
+
+    // Iterate over each set of pages in the list, then iterate over each rule in
+    // the rules list. If both entries in the current rule exist in the current 
+    // set of pages, check their order within the current set, and if it's correctly
+    // sorted, add the middle element from that vector to an accumulator that Rust
+    // so conveniently provides for us. Then return the accumulator.
     pages
         .into_iter()
         .filter(|page| {
@@ -28,6 +34,10 @@ pub fn count_correct_pages(rules: Vec<(u32, u32)>, pages: Vec<Vec<u32>>) -> u32 
         .sum()
 }
 
+// Checks if two entries in a page list are correctly sorted. This should get passed the
+// current page list, and an x and y corresponding to a specific rule from the rule list,
+// where x is the first number and y is the second, indicating that y should come after
+// x in the page list.
 fn check_order(page: Vec<u32>, x: u32, y: u32) -> bool {
     let x_pos = page.iter().position(|n| *n == x).unwrap();
     let y_pos = page.iter().position(|n| *n == y).unwrap();
@@ -35,6 +45,7 @@ fn check_order(page: Vec<u32>, x: u32, y: u32) -> bool {
     y_pos > x_pos
 }
 
+// Process puzzle input into the appropriate rules and pages vectors
 fn process_input(input: Vec<String>) -> (Vec<(u32, u32)>, Vec<Vec<u32>>) {
     let split_index = input.iter().position(|s| s.is_empty()).unwrap();
     let (rules, pages) = input.split_at(split_index);
@@ -60,6 +71,7 @@ fn process_input(input: Vec<String>) -> (Vec<(u32, u32)>, Vec<Vec<u32>>) {
 
     (r, p)
 }
+
 
 #[cfg(test)]
 mod tests {
