1cb788231fd37de7402cc5885bc9538136893852
Joe Abernathy <jabernathy36@gmail.com>
2025-12-01 16:58:53 -0600

    Refactored a bit, made part 1 a bit more simple

--- 2025/day-01/src/bin/part1.rs
+++ 2025/day-01/src/bin/part1.rs
@@ -1,22 +1,6 @@
 use aoc_helpers::{ * };
 use std::error::Error;
 
-// Custom enum for handling directions. Overkill? Probably, but here we are.
-#[derive(Debug)]
-enum Dir { L, R }
-
-// Implementing TryFrom to allow conversion from char ('L' or 'R') to Dir
-impl TryFrom<char> for Dir {
-    type Error = ();
-
-    fn try_from(ch: char) -> Result<Self, Self::Error> {
-        match ch {
-            'L' => Ok(Dir::L),
-            'R' => Ok(Dir::R),
-            _ => Err(()),
-        }
-    }
-}
 
 fn main() -> Result<(), Box<dyn Error>> {
     let input_lines = get_puzzle_input_as_lines(2025, 1)?;
@@ -27,25 +11,32 @@ fn main() -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
-fn parse_input(input: Vec<String>) -> Vec<(Dir, u32)> {
+fn parse_input(input: Vec<String>) -> Vec<i32> {
     input.iter()
-         .map(|line| {
+        .map(|line| {
             let (dir, ticks) = line.split_at(1);
-            (Dir::try_from(dir.chars().next().unwrap()).expect("Invalid direction character"), ticks.parse::<u32>().unwrap())
+            let sign = match dir.chars().next().unwrap() {
+                'L' => -1,
+                'R' => 1,
+                _ => panic!("Invalid direction character: {}", dir),
+            };
+            sign * ticks.parse::<i32>().unwrap()
          })
-         .collect()
+        .collect()
 }
 
-fn count_zeros(input: Vec<(Dir, u32)>) -> u32 {
+fn count_zeros(input: Vec<i32>) -> u32 {
     let mut dial: i32 = 50;
     let mut zero_count: u32 = 0;
 
-    for (dir, ticks) in input {
-        
-        // Either add or subtract the current number of ticks to dial, and do some modulo math to keep the result between 0 and 99
-        match dir {
-            Dir::L => dial = (((dial - ticks as i32) % 100) + 100) % 100,
-            Dir::R => dial = (dial + ticks as i32) % 100,
+    for ticks in input {
+        dial += ticks;
+
+        if dial > 99 {
+            dial %= 100;
+
+        } else if dial < 0 {
+            dial = ((dial % 100) + 100) % 100
         }
 
         if dial == 0 {
